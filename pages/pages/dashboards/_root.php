1fec4c1f436af15ebcfb25f87a13a1f05d6b1016
Alisson Almeida <aalissonalmeidaq@gmail.com>
2021-11-29 23:11:56 -0300

    painel de tarefas no dashboard

--- pages/pages/dashboards/_root.php
+++ pages/pages/dashboards/_root.php
@@ -24,13 +24,13 @@
 
     <div class="row">
       <div class="col-12 col-sm-6 col-md-4">
-        <a href="#">
+        <a class="nav-link" href="#todayTask" data-toggle="tab" onclick="window.getElementById('todayTask').classList.add(' active');">
 
           <div class="small-box bg-default">
             <div class="inner mx-3">
               <?php
-              $today = date("Y-m-d", time());
-              $sql = "SELECT * FROM tarefas WHERE dtTarefa = '$today'";
+              //$today = date("Y-m-d", time());
+              $sql = "SELECT * FROM tarefas WHERE dtTarefa =  CURDATE()";
               $resultado = $conexao->query($sql);
               ?>
 
@@ -50,7 +50,7 @@
       <div class="col-12 col-sm-6 col-md-4">
 
 
-        <a href="">
+        <a class="nav-link" href="#allTask" data-toggle="tab" onclick="window.getElementById('todayTask').classList.add(' active');">
           <div class="small-box bg-gradient-default">
             <div class="inner mx-3">
               <?php
@@ -105,6 +105,242 @@
       <div class="col-md-4">
         <div id='calendar'></div>
       </div>
+      <!-- /.col -->
+      <div class="col-md-8">
+        <div class="">
+          <!-- <h4>Tarefas</h4> -->
+          <ul class="nav nav-pills mt-3 mb-1">
+            <li class="nav-item"><a class="nav-link" href="#todayTask" data-toggle="tab">
+                <i class="align-middle mdi mdi-calendar-multiple mdi-24px fa fa-fw"> </i>&nbsp;&nbsp;
+                <span class="align-middle">
+                  Tarefas de hoje
+                </span>
+              </a>
+            </li>
+
+
+            <li class="nav-item">
+              <a class="nav-link " href="#allTask" data-toggle="tab">
+                <i class="align-middle mdi mdi-calendar-clock mdi-24px fa fa-fw"></i>&nbsp;&nbsp;
+                <span class="align-middle">
+                  Tarefas Agendadas
+                </span>
+              </a>
+            </li>
+
+            <!-- <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a>
+                </li> -->
+
+          </ul>
+        </div>
+        <div class="card">
+          <div class="card-tools">
+            <!-- <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+              </button> -->
+            <button type="button" class="btn btn-default float-right m-2 "><i class="fas fa-plus"></i> Nova Tarefa
+            </button>
+
+          </div>
+          <!-- <div class="card-header">
+            <h3 class="card-title"></h3>
+
+
+
+          </div> -->
+          <!-- /.card-header -->
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-12">
+
+                <div class="tab-content" id="pills-tabContent">
+                  <div class="tab-pane fade show active" id="todayTask" role="tabpanel" aria-labelledby="todayTask-tab">
+                    <!-- Tarefas de hoje -->
+                    <div class="table-responsive ">
+                      <table id="tabela" class="table table-sm table-striped table-hover">
+                        <thead class="" style="font-family: 'Advent Pro', sans-serif;">
+                          <tr>
+                            <th class="col-md-1 text-center align-middle">
+                              <i class="fas fa-hashtag fa-fw"></i>
+                            </th>
+                            <th class="col-md-4 text-center align-middle ">Tarefa</th>
+                            <th class="col-md-2 text-center align-middle ">status</th>
+                            <th class="col-md-3 text-center align-middle ">Responsável</th>
+                            <th class="col-md-2 text-center align-middle ">Data e Hora</th>
+                            <th class="col-md-auto text-center align-middle">
+                              <i class="fab fa-lg fa-fw fa-whmcs" title="Ações"></i>
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <?php
+                          $sql = "SELECT * FROM tarefas WHERE finalizada = 0 AND dtTarefa = CURDATE() OR dtTarefa < CURDATE()  ORDER BY dtTarefa ASC";
+                          $resultado = $conexao->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                          $count = 1;
+                          foreach ($resultado as $task) {
+                          ?>
+                            <tr class="">
+                              <td class=" text-uppercase align-middle text-center" style="font-size: .9rem;">
+                                <?php echo str_pad($count, 3, "0", STR_PAD_LEFT);
+                                $count++;
+                                ?>
+                              </td>
+                              <td class="text-uppercase align-middle">
+                                <?= $task['decricaoTarefa']; ?>
+                              </td>
+                              <td class="text-uppercase align-middle text-center" style="font-weight: 300;">
+                                <?php
+                                $today = date("Y-m-d", time());
+                                if ($task['dtTarefa'] < $today) {
+                                  echo "<span class='badge badge-pill badge-danger px-2 py-1'>Atrasada</span>";
+                                } else {
+                                  echo "<span class='badge badge-pill badge- px-2 py-1'>Hoje</span>";
+                                }
+                                ?>
+                              </td>
+
+                              <td class=" text-uppercase align-middle" style="font-size: .8rem; ">
+                                <?php
+                                $sql = "SELECT * FROM pessoa WHERE idPessoa = '" . $task['idResponsavel'] . "'";
+                                $resultado = $conexao->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                                foreach ($resultado as $pessoa) {
+                                  echo $pessoa['nmPessoa'];
+                                }
+                                ?>
+
+                              </td>
+
+                              <td class="text-uppercase align-middle text-center">
+                                <?php
+                                echo date('d/m/Y', strtotime($task['dtTarefa']));
+                                echo " AS ";
+                                echo date('H:i', strtotime($task['hora']));
+                                ?>
+
+
+                              </td>
+
+                              <td class="text-uppercase align-middle  ">
+                                <ul class="nav justify-content-center d-flex justify-content-evenly">
+
+                                  <li class="nav-item">
+                                    <a href="" class="btn btn-tool" target="" title="Visializar Processo" rel="noopener noreferrer">
+                                      <i class="mdi mdi-file-eye-outline mdi-24px "></i>
+                                    </a>
+                                  </li>
+
+                                </ul>
+                              </td>
+                            </tr>
+                          <?php } ?>
+
+                        </tbody>
+                      </table>
+                    </div>
+                    <!-- /.tarefas -->
+
+                  </div>
+                  <div class="tab-pane fade" id="allTask" role="tabpanel" aria-labelledby="allTask-tab">
+
+                    <!-- Todas as Tarefas  -->
+                    <div class="table-responsive ">
+                      <table id="tabela" class="table table-sm table-striped table-hover">
+                        <thead class="" style="font-family: 'Advent Pro', sans-serif;">
+                          <tr>
+                            <th class="col-md-1 text-center align-middle">
+                              <i class="fas fa-hashtag fa-fw"></i>
+                            </th>
+                            <th class="col-md-4 text-center align-middle ">Tarefa</th>
+                            <th class="col-md-2 text-center align-middle ">status</th>
+                            <th class="col-md-3 text-center align-middle ">Responsável</th>
+                            <th class="col-md-2 text-center align-middle ">Data e Hora</th>
+
+                            <th class="col-md-auto text-center align-middle">
+                              <i class="fab fa-lg fa-fw fa-whmcs" title="Ações"></i>
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <?php
+                          $sql = "SELECT * FROM tarefas WHERE finalizada = 0 ORDER BY dtTarefa ASC";
+                          $resultado = $conexao->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                          $count = 1;
+                          foreach ($resultado as $task) {
+                          ?>
+                            <tr class="">
+                              <td class=" text-uppercase align-middle text-center" style="font-size: .9rem;">
+                                <?php echo str_pad($count, 3, "0", STR_PAD_LEFT);
+                                $count++;
+                                ?>
+                              </td>
+                              <td class="text-uppercase align-middle">
+                                <?= $task['decricaoTarefa']; ?>
+                              </td>
+                              <td class="text-uppercase align-middle text-center" style="font-weight: 300;">
+                                <?php
+                                $today = date("Y-m-d", time());
+                                if ($task['dtTarefa'] < $today) {
+                                  echo "<span class='badge badge-pill badge-danger px-2 py-1'>Atrasada</span>";
+                                } elseif ($task['dtTarefa'] == $today) {
+                                  echo "<span class='badge badge-pill badge-orange  px-2 py-1'>Hoje</span>";
+                                } else {
+                                  echo "<span class='badge badge-pill badge-success px-2 py-1'>Futura</span>";
+                                }
+                                ?>
+                              </td>
+
+                              <td class="text-uppercase align-middle" style="font-size: .8rem; ">
+                                <?php
+                                $sql = "SELECT * FROM pessoa WHERE idPessoa = '" . $task['idResponsavel'] . "'";
+                                $resultado = $conexao->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                                foreach ($resultado as $pessoa) {
+                                  echo $pessoa['nmPessoa'];
+                                }
+                                ?>
+
+                              </td>
+
+                              <td class="text-uppercase align-middle text-center">
+                                <?php
+                                echo date('d/m/Y', strtotime($task['dtTarefa']));
+                                echo " AS ";
+                                echo date('H:i', strtotime($task['hora']));
+                                ?>
+                              </td>
+
+                              <td class="text-uppercase align-middle  ">
+                                <ul class="nav justify-content-center d-flex justify-content-evenly">
+
+                                  <li class="nav-item">
+                                    <a href="" class="btn btn-tool" target="" title="Visializar Processo" rel="noopener noreferrer">
+                                      <i class="mdi mdi-file-eye-outline mdi-24px "></i>
+                                    </a>
+                                  </li>
+
+                                </ul>
+                              </td>
+                            </tr>
+                          <?php } ?>
+
+                        </tbody>
+                      </table>
+                    </div>
+                    <!-- /.tarefas -->
+
+                  </div>
+
+                </div>
+                <!-- ./chart-responsive -->
+              </div>
+              <!-- /.col -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+
+
       <!-- /.col -->
     </div>
     <!-- /.row -->
